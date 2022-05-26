ad4fcce7ea696bac3b04703d4cbbbea76bdfd07d
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2022-05-26 21:45:46 +0300

    Add search

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
-
-const cubes = require('../db.json');
+const cubeService = require('../services/cubeService');
 
 router.get('/', (req, res) => {
+    let { search, from, to } = req.query;
+
+    const cubes = cubeService.getAll(search, from, to);
+
     res.render('index', { cubes });
 });
 
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -3,10 +3,18 @@ const path = require('path');
 
 const cubes = require('../db.json');
 
+exports.getAll = (search = '', from = 0, to = 6) => {
+    const result = cubes
+        .filter(x => x.name.toLowerCase().includes(search.toLowerCase()))
+        .filter(x => x.difficultyLevel >= from && x.difficultyLevel <= to);
+
+    return result;
+};
+
 exports.getOne = (cubeId) => cubes[cubeId];
 
 exports.save = (cube) => {
-    cubes.push({id: cubes[cubes.length - 1].id + 1, ...cube});
+    cubes.push({ id: cubes[cubes.length - 1].id + 1, ...cube });
 
     let textData = JSON.stringify(cubes, '', 4);
 
