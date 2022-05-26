aca465da92c62d43e623c524614e30f1148050a4
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2022-05-26 18:52:28 +0300

    Render home page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
 app.use('/static', express.static('public'));
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('index');
 });
 
 app.listen(5000, () => console.log(`App is listening on port 5000`));
