8d8cbbc8c3c3eacfe675f9117cc03dcac957c887
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2022-05-26 19:39:29 +0300

    Add about page and fix navigation

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,7 @@
 exports.index = (req, res) => {
     res.render('index');
-}
+};
+
+exports.about = (req, res) => {
+    res.render('about');
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 const experss = require('express');
 
 const homeController = require('./controllers/homeController');
+const cubeController = require('./controllers/cubeController');
 
 const router = experss.Router();
 
 router.get('/', homeController.index);
+router.get('/about', homeController.about);
+
+router.use('/cube', cubeController);
 
 module.exports = router;
