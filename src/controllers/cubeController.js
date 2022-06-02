4050a53e7cd6ebcf72ed128dd923313271edb5c9
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2022-06-02 21:28:26 +0300

    Show accessories in the details page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -28,7 +28,7 @@ router.post('/create', async (req, res) => {
 
 router.get('/details/:id', async (req, res) => {
     const cube = await cubeService.getOne(req.params.id).lean();
-
+    
     res.render('details', { cube });
 });
 
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -18,7 +18,16 @@ exports.getAll = async (search = '', fromInput, toInput) => {
     return cubes;
 };
 
-exports.getOne = (cubeId) => Cube.findById(cubeId);
+exports.getOne = (cubeId) => Cube.findById(cubeId).populate('accessories');
+
+// Nested population
+// .populate({
+//     path: 'accessories',
+//     populate: {
+//         path: 'cubes',
+//         model: 'Cube'
+//     }
+// });
 
 exports.create = (cube) => Cube.create(cube);
 
