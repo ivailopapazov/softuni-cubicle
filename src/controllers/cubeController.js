8861f4422010fedad52d6711328c55b9779491af
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2022-06-02 20:29:34 +0300

    Add dynamic rendering for accessory select dropdown

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const cubeService = require('../services/cubeService');
+const accessoryService = require('../services/accessoryService');
 
 router.get('/create', (req, res) => {
     res.render('create');
@@ -33,8 +34,9 @@ router.get('/details/:id', async (req, res) => {
 
 router.get('/:cubeId/attach-accessory', async (req, res) => {
     const cube = await cubeService.getOne(req.params.cubeId).lean();
+    const accessories = await accessoryService.getAll().lean();
 
-    res.render('accessory/attach', {cube}); 
+    res.render('accessory/attach', {cube, accessories}); 
 });
 
 module.exports = router;
