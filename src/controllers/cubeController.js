a1d2bdc2ae91f8ea09224ea89a39744603a8ab20
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2022-06-02 20:23:30 +0300

    Add dynamic content to attach accessory page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -31,8 +31,10 @@ router.get('/details/:id', async (req, res) => {
     res.render('details', { cube });
 });
 
-router.get('/:cubeId/attach-accessory', (req, res) => {
-    res.render('accessory/attach'); 
+router.get('/:cubeId/attach-accessory', async (req, res) => {
+    const cube = await cubeService.getOne(req.params.cubeId).lean();
+
+    res.render('accessory/attach', {cube}); 
 });
 
 module.exports = router;
