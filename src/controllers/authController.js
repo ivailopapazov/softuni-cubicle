045a53c4ed915a285fe3fbfae4763e4983c695b1
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2022-06-09 20:15:14 +0300

    Create jwt token on successful login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -22,9 +22,14 @@ router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
-router.post('/login', (req, res) => {
-    console.log(req.body); 
-    res.end();
+router.post('/login', async (req, res) => {
+    let token = await authService.login(req.body);
+    console.log(token);
+    if (!token) {
+        return res.redirect('/404');
+    }
+
+    res.redirect('/');
 });
 
 module.exports = router;
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,8 +1,10 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 
 const saltRounds = 10;
+const secret = 'alsdkjfaposdl;fgijhsdaklgjfhaskdlfghjasdklfgjh';
 
 exports.register = async ({ username, password, repeatPassword }) => {
     // TODO: return form validation message
@@ -16,7 +18,7 @@ exports.register = async ({ username, password, repeatPassword }) => {
         username,
         password: hashedPassword,
     });
-    
+
     // let createdUser = new User({
     //     username,
     //     password: hashedPassword,
@@ -25,4 +27,31 @@ exports.register = async ({ username, password, repeatPassword }) => {
     // createdUser.save();
 
     return createdUser;
+};
+
+exports.login = async ({ username, password }) => {
+    let user = await User.findOne({ username });
+
+    if (!user) {
+        // TODO: add message
+        return;
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        return;
+    }
+
+    let result = new Promise((resolve, reject) => {
+        jwt.sign({ _id: user._id, username: user.username }, secret, { expiresIn: '2d' }, (err, token) => {
+            if (err) {
+                return reject(err);
+            }
+    
+            resolve(token);
+        });
+    });
+
+    return result;
 };
